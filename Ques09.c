87b58980128d15c1d67d2c99a507c578776e76eb
sarwar1ahammed <110556391+sarwar1ahammed@users.noreply.github.com>
2022-08-26 22:35:25 +0530

    Add files via upload

--- Ques09.c
+++ Ques09.c
@@ -9,15 +9,15 @@ int main()
     printf("Enter three numbers : ");
     scanf("%d %d %d", &num1, &num2, &num3);
 
-    if (num1 >= num2 && num1 >= num3)
+    if (num1 > num2 && num1 > num3)
     {
         printf("Greater number is : %d", num1);
     }
-    else if (num2 >= num1 && num2 >= num3)
+    else if (num2 > num3)
     {
         printf("Greater number is : %d", num2);
     }
-    else if (num3 >= num1 && num3 >= num2)
+    else 
     {
         printf("Greater number is : %d", num3);
     }
